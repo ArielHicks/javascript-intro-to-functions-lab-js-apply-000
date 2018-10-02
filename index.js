5e93801d25760b0e46de1504a26fcb25e000bdaf
Ariel Hicks <hicks.ariel@gmail.com>
2018-10-02 19:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log (string.toUpperCase('HELLO'))
 }
+
 function logWhisper(string) {
   console.log (string.toLowerCase('I can\'t hear you!'))
 }
+
 function sayHiToGrandma(string) {
 	if (string === string.toUpperCase()) {
   		return "YES INDEED!"
